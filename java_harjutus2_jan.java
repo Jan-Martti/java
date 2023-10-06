6650263a565ace483342700a976a5b7e9a854ce9
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-10-06 08:56:28 +0300

    Add files via upload

---
+++ java_harjutus2_jan.java
@@ -0,0 +1,24 @@
+ public class java_harjutsu2_jan {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 39.370f;
+        float jagamine;
+
+        jagamine = a / b;
+
+        System.out.println(jagamine + "  meetrit");
+        number2();
+    }
+
+    static void number2(){
+
+        int c = 6;
+        int d = 3;
+        float pi = 3.14f;
+
+        float arv = c*d*pi;
+        System.out.println( arv + " blö");
+    }
+}
+
+    
---
+++ java_harjutused_jan.java
@@ -0,0 +1,13 @@
+public class java_harjutused_jan {
+1ˇ1     public static void main(String[] args) {
+    
+    String eesnimi = new String();
+    eesnimi = "jan";
+
+    String klass = new String();
+     klass = "teine kursus";
+
+    System.out.println(eesnimi +" "+ klass);
+    }
+}
+    
