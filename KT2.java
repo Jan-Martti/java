7a10f839bef9060d9e5d9b4cc8227ee4002236ae
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-10-06 10:08:41 +0300

    Update KT2.java

--- KT2.java
+++ KT2.java
@@ -4,8 +4,9 @@
 public class KT2 {
     public static void main(String[] args) {
     for (int i = 1; i <= 66; i++) {
-    if (i % 3 == 0) {
-    System.out.print(i + ", ");
+    if (i % 3 == 0) { 
+    System.out.print(i + ", "); // Väljastab praeguse i väärtuse koos koma ja tühikuga
+    
             }
         }
     System.out.println();
