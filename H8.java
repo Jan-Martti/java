9b915bbbc11b6969277d7cb0ae42c4a3ddd8a458
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-10-10 00:01:06 +0300

    Update H8.java

--- H8.java
+++ H8.java
@@ -1,91 +1,56 @@
 //Jan-Martti Olop
 //Harjutus 8
 import java.util.Scanner;
-
 public class H8 {
+
+	private static Scanner scanner = new Scanner (System.in);
+
 	public static void main(String[] args) {
 
-		String laius = "";
-		String alus = "";
-		String korgus = "";
-		String pindala = "";
-		Scanner scanner = new Scanner(System.in);
-		System.out.print(
-				"Rööpküliku pindala (1) \nRistküliku pindala (2) \nKolmnurga pindala (3) \nRööptahukas ruumala (4) \n Risttahuka ruumala (5) \nKuubi ruumala (6)\nVali tegevus: ");
+		System.out.print("Rööpküliku pindala (1) \nRistküliku pindala (2) \nKolmnurga pindala (3) \nRisttahuka ruum (4) \nparallelepipedi ruum  (5) \nRistküliku ruum (6) \nVali tegevus (7): "); 
 		String valik = scanner.next();
 
-		if (valik.equals("5")) {
-			System.out.print("Sisesta laius: ");
-			laius = scanner.next();
-			System.out.print("Sisesta alus: ");
-			alus = scanner.next();
-			System.out.print("Sisesta kõrgus: ");
-			korgus = scanner.next();
-
-		} else if (valik.equals("6")) {
-			System.out.print("Sisesta alus: ");
-			alus = scanner.next();
-
-		} else if (valik.equals("1")) {
-			System.out.print("Sisesta kõrgus: ");
-			korgus = scanner.next();
-			System.out.print("Sisesta alus: ");
-			alus = scanner.next();
-
-		} else if (valik.equals("4")) {
-			System.out.print("Sisesta põhja pindala: ");
-			pindala = scanner.next();
-			System.out.print("Sisesta kõrgus: ");
-			korgus = scanner.next();
-
-		} else if (valik.equals("2")) {
-			System.out.print("Sisesta kõrgus: ");
-			korgus = scanner.next();
-			System.out.print("Sisesta alus: ");
-			alus = scanner.next();
-
-		} else if (valik.equals("3")) {
-			System.out.print("Sisesta kõrgus: ");
-			korgus = scanner.next();
-			System.out.print("Sisesta alus: ");
-			alus = scanner.next();
-		}
-
+		
+		System.out.print("Sisesta alus: ");
+		String alus = scanner.next();
+		System.out.print("Sisesta kõrgus: ");
+		String korgus = scanner.next();
+		System.out.print("Sisesta laius: ");
+		String laius = scanner.next();
+		scanner.close();
 		
 
 		double s;
 		switch (Integer.parseInt(valik)) {
-			case 1:
-				s = Pindalad.roopkylik(alus, korgus);
-				System.out.printf("Rööpküliku pindala on %.2f ", s);
-				break;
-			case 2:
-				s = Pindalad.ristkylik(alus, korgus);
-				System.out.printf("Ristküliku pindala on %.2f ", s);
-				break;
-			case 3:
-				s = Pindalad.kolmnurk(alus, korgus);
-				System.out.printf("Kolmnurga pindala on %.2f ", s);
-				break;
-			case 4:
-				s = ruumalad.roopkylk(pindala, korgus);
-				System.out.printf("Rööptahuka ruumala on %.2f m kuubis", s);
-				break;
-			case 5:
-				s = ruumalad.risttahukas(alus, korgus, laius);
-				System.out.printf("Risttahuka ruumala on %.2f m kuubis", s);
-				break;
-			case 6:
-				s = ruumalad.kuup(alus);
-				System.out.printf("Kuubi ruumala on %.2f  m kuubis ", s);
-				break;
+		case 1:
+			s = Pindalad.roopkylikS(alus, korgus);
+			System.out.printf("Rööpküliku pindala on %.2f ",s); //see prindib välja rööpküliku valemi vastuse mis paned consoli kui tahab suuruseid
+			break;
+		case 2:
+			s = Pindalad.ristkylikS(alus, korgus);
+			System.out.printf("Ristküliku pindala on %.2f ",s); //See prindib välja ristküliku valemi
+			break;
+		case 3:
+			s = Pindalad.kolmnurkS(alus, korgus);
+			System.out.printf("Kolmnurga pindala on %.2f ",s); //See prindib välja kolmnurga valemi
+			break;
+        case 4:
+            s = Ruumalad.ristahuka(alus, korgus, laius);
+            System.out.printf("Risttahuka ruumala on %.2f ",s);
+            break;
+		case 5:
+			s = Ruumalad.parallelepipedi(alus, korgus, laius);
+			System.out.printf("Risttahuka ruumala on %.2f ",s);
+            break;
+		case 6:
+			s = Ruumalad.Ristkülik(alus, korgus, laius);
+			System.out.printf("Ristküliku ruumala on %.2f ",s);
+			break;
 
-			default:
-				System.out.print("Valikust arusaamine ebaõnnestus!");
-				break;
+		default:
+			System.out.print("Valikust arusaamine ebaõnnestus!"); //see annab errori kui ei valinud mida teha tahaksid
+			break;
+
 		}
-
-		scanner.close();
 	}
-
 }
