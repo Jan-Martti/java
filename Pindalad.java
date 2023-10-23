9e62d5adcf5d40c6071c885b5f44099056df102b
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-10-23 12:58:05 +0300

    Add files via upload

---
+++ Pindalad.java
@@ -0,0 +1,18 @@
+
+public class Pindalad {
+
+    
+
+    public static double kolmnurkS(double alus, double korgus) {
+        return 0;
+    }
+
+    public static double ristkylikS(double alus, double korgus) {
+        return 0;
+    }
+
+    public static double roopkylikS(double alus, double korgus) {
+        return 0;
+    }
+
+}
