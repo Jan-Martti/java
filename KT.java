9f58698914cc27569445419195846db4add80d1a
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-10-24 02:22:02 +0300

    Update KT.java

--- KT.java
+++ KT.java
@@ -4,11 +4,22 @@
 
 public class KT {
     public static void main(String[] args) {
-        for (int i = 1; i <= 66; i++) {
-            System.out.print(i);
+        try {
+            // Kutsutakse alamprogrammi, et prindiks arvud ja käidelda erandeid
+            printNumbers();
+        } catch (Exception e) { // Käideldakse erandeid, mis võivad tekkida
+            System.out.println("Erind tekkis: " + e); // Prinditakse erandi teade koos üksikasjadega
         }
-        System.out.println();
+    }
+
+    // Alamprogramm, mis prindib arvud 1-66 või mis iganes sa valid 
+    public static void printNumbers() {
+        for (int i = 1; i <= 66; i++) {
+            System.out.print(i); // Prinditakse arv
+        }
+        System.out.println(); // Liigutakse uuele reale
     }
 }
+
 
 
