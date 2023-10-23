37868c5715b6834759ba139dd794cd8b028d3463
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-10-23 13:06:34 +0300

    Update Ruumalad.java

--- Ruumalad.java
+++ Ruumalad.java
@@ -1,18 +1,20 @@
-
-
-public class Ruumalad {
-
-
-    public static double parallelepipedi(double alus, double korgus, double laius) {
-        return 0;
+blic class Ruumalad {
+    public static double roopkylk(String u, String h) {
+        float ppindala = Float.parseFloat(u);
+        float korgus = Float.parseFloat(h);
+        double s = ppindala * korgus;
+        return s;
     }
 
-    public static double ristahuka(double alus, double korgus, double laius) {
-        return 0;
+    public static double risttah(String a, String h, String c) {
+        float alus = Float.parseFloat(a);
+        float korgus = Float.parseFloat(h);
+        float laius = Float.parseFloat(c);
+        double s = alus * korgus * laius;
+        return s;
     }
 
-    public static double Ristkülik(double alus, double korgus, double laius) {
-        return 0;
-    }
-
-}
+    public static double kuup(String a) {
+        float alus = Float.parseFloat(a);
+        double s = alus * alus * alus;
+        return s;
