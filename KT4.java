eefd48d7e3d9c6beb5dd40ccbeb7e877206af57f
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-10-24 02:20:11 +0300

    Update KT4.java

--- KT4.java
+++ KT4.java
@@ -3,16 +3,20 @@
 
 public class KT4 {
     public static void main(String[] args) {
-        int arv = 1;
+        try {
+            int arv = 1; // Algab muutuja 'arv' väärtusega 1
 
-        while (arv <= 66) {
-            for (int i = 0; i < 5; i++) {
-                if (arv <= 66) {
-                    System.out.print(arv);
-                    arv++;
+            while (arv <= 66) { // See on while-tsükkel, mis jätkub niikaua kui 'arv' on väiksem või võrdne 66-ga
+                for (int i = 0; i < 5; i++) { // See on pesitsusfor-tsükkel, mis kordub 5 korda
+                    if (arv <= 66) { // Kontrollitakse, kas 'arv' on endiselt väiksem või võrdne 66-ga
+                        System.out.print(arv); // Prinditakse 'arv' praegune väärtus ilma rea vahetamata
+                        arv++; // Suurendatakse 'arv' väärtust 1 võrra
+                    }
                 }
+                System.out.println(); // Liigutatakse uuele reale pärast numbreid pesitsusfor-tsükli sees
             }
-            System.out.println();
+        } catch (Exception e) { // Käideldakse võimalikke erandeid
+            System.out.println("Erind tekkis: " + e); // Prinditakse teade, kui erand tekib, koos erandi üksikasjadega
         }
     }
 }
