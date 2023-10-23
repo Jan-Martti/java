15a920461809de970070e3a35e02bbbef458e95a
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-10-24 02:20:25 +0300

    Update KT3.java

--- KT3.java
+++ KT3.java
@@ -8,13 +8,23 @@ public class KT3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        try {
+            // Kutsutakse alamprogrammi, et teisendada kümnendsüsteemi arv kahendsüsteemi ja käidelda erandeid
+            convertDecimalToBinary(scanner);
+        } catch (Exception e) { // Käideldakse erandeid, mis võivad tekkida
+            System.out.println("Erind tekkis: " + e); // Prinditakse erandi teade koos üksikasjadega
+        } finally {
+            scanner.close(); // Scanner tuleb lõpuks sulgeda
+        }
+    }
+
+    // Alamprogramm, mis teisendab kümnendsüsteemi arvu kahendsüsteemi
+    public static void convertDecimalToBinary(Scanner scanner) {
         System.out.print("Sisesta kümnendsüsteemi arv: ");
-        int decimal = scanner.nextInt();
+        int decimal = scanner.nextInt(); // Loetakse sisend kasutades scannerit
 
-        String binary = Integer.toBinaryString(decimal);
+        String binary = Integer.toBinaryString(decimal); // Teisendatakse arv kahendsüsteemi
 
-        System.out.println("Kahendsüsteemi teisendus: " + binary);
-
-        scanner.close();
+        System.out.println("Kahendsüsteemi teisendus: " + binary); // Prinditakse teisendatud arv
     }
 }
