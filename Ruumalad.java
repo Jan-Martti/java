a8ee16c54229123cf38ab4e0391114f1be0cca55
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-10-23 12:58:15 +0300

    Add files via upload

---
+++ Ruumalad.java
@@ -0,0 +1,18 @@
+
+
+public class Ruumalad {
+
+
+    public static double parallelepipedi(double alus, double korgus, double laius) {
+        return 0;
+    }
+
+    public static double ristahuka(double alus, double korgus, double laius) {
+        return 0;
+    }
+
+    public static double Ristkülik(double alus, double korgus, double laius) {
+        return 0;
+    }
+
+}
